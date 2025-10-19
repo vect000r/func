dc4df2ec56cf70883215e3051cc498e02b236832
vect0r <vectoreddd@gmail.com>
2025-10-19 21:56:38 +0200

    feat: 3.0 ex

--- ex1/src/main/scala/main.scala
+++ ex1/src/main/scala/main.scala
@@ -1,14 +1,31 @@
 package org.func.ex1
 
 import cask.MainRoutes
+import upickle.core.BufferedValue.False
 
 
 object Main extends cask.MainRoutes:
 
-  @cask.postJson("/input")
-  def dataEndpoint(nums: List[Int]): String =
-    "Nums: " + nums.toString
-  
+  val sortOrder: (Int, Int) => Boolean = (x: Int, y: Int) => x > y
+
+  @cask.postJson("/is-sorted")
+  def checkSorted(nums: List[Int]): ujson.Obj =
+    val sorted = isSorted(nums, sortOrder)
+    ujson.Obj(
+      "input" -> nums,
+      "isSorted" -> sorted,
+    )
+
+  def isSorted(input: List[Int], function: (Int, Int) => Boolean ): Boolean = {
+    val pairs = input.sliding(2).map {
+      case List(x, y) => (x, y)
+    }.toList
+
+    pairs.forall {
+      case (first, second) => function(first, second)
+    }
+  }
+
 
 
   initialize()
