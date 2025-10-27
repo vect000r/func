cfbd4388d45d718cb3f469ca9ccdbc8ad200dcea
vect0r <vectoreddd@gmail.com>
2025-10-27 16:33:25 +0100

    feat: docker support

--- ex1/src/main/scala/Main.scala
+++ ex1/src/main/scala/Main.scala
@@ -5,6 +5,8 @@ import upickle.core.BufferedValue.False
 
 
 object Main extends cask.MainRoutes:
+  override def host = "0.0.0.0"
+  override def port = 8080
 
   val sortOrder: (Int, Int) => Boolean = (x: Int, y: Int) => x > y
 
