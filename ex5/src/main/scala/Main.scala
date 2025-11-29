c0c498f5a9cbc8d05731f08db9525a3c436998b2
vect0r <vectoreddd@gmail.com>
2025-11-29 16:19:47 +0100

    feat: ex5 TODO

--- ex5/src/main/scala/Main.scala
+++ ex5/src/main/scala/Main.scala
@@ -8,6 +8,34 @@ object Main extends cask.MainRoutes:
   override def host = "0.0.0.0"
   override def port = 8080
 
+  @cask.postJson("/dict")
+  def getDict(list: List[Int]): ujson.Obj = {
+    // TODO
+    ???
+  }
 
+  @cask.postJson("/dict3")
+  def getDict3(list: List[Int]): ujson.Obj = {
+    // TODO
+    ???
+  }
+
+  @cask.postJson("/dictStudent")
+  def getDictStudent(list: List[Int]): ujson.Obj = {
+    // TODO
+    ???
+  }
+
+  @cask.postJson("/dict")
+  def getDictVector(list: List[Int]): ujson.Obj = {
+    // TODO
+    ???
+  }
+
+  @cask.postJson("/dict")
+  def getDictGrades(indices: List[Int], grades: List[Int]): ujson.Obj = {
+    // TODO
+    ???
+  }
 
   initialize()
