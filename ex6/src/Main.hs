cef65334c984f518caddca74c2aba82cd985f68f
vect0r <vectoreddd@gmail.com>
2025-12-28 17:20:25 +0100

    feat: ex6 3.0 first version

--- ex6/src/Main.hs
+++ ex6/src/Main.hs
@@ -1,10 +1,48 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Main (main) where
+module Main where
 
 import Web.Scotty
+import Data.Aeson (FromJSON, ToJSON, object, (.=), toJSON)
+import GHC.Generics
+import Network.Wai.Middleware.Cors
 
+
+
+-- Request types
+data IsSortedRequest = IsSortedRequest
+    { nums :: [Int]
+    } deriving (Show, Generic)
+
+instance FromJSON IsSortedRequest
+instance ToJSON IsSortedRequest
+
+
+-- Helper functions
+isSorted:: [Int] -> (Int -> Int -> Bool) -> Bool
+isSorted [] _ = True
+isSorted [_] _ = True
+isSorted (x:y:rest) f = f x y && isSorted (y:rest) f
+
+sortOrder :: Int -> Int -> Bool
+sortOrder x y = x > y
+
+-- Endpoint handlers
+isSortedHandler :: ActionM ()
+isSortedhandler = do 
+    req <- jsonData :: ActionM IsSortedRequest 
+    let inputList = nums req
+    let sorted = isSorted inpuList sortOrder
+    json $ object 
+        [
+            "input" .= inputList,
+            "isSorted" .= sorted
+        ]
+
+-- Main
 main :: IO ()
-main = scotty 3000 $ do
-    get "/" $ do
-        text "Hello from Haskell + Scotty!"
+main = scotty 8080 $ do
+    middleware simpleCors
+
+    post "/is-sorted" isSortedHandler
