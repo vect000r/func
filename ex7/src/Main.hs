d2318cb1e9d707f9a04c675c4a97f87407ae9fad
vect0r <vectoreddd@gmail.com>
2026-01-02 12:47:16 +0100

    feat: ex7 3.5 finished

--- ex7/src/Main.hs
+++ ex7/src/Main.hs
@@ -18,10 +18,22 @@ data RandomIntResponse = RandomIntResponse
 
 instance ToJSON RandomIntResponse
 
+data RandomDoubleResponse = RandomDoubleResponse
+    { valueDouble :: Double
+    } deriving (Show, Generic)
+
+instance ToJSON RandomDoubleResponse
 
 -- Helper functions
 randomInt :: IO Int
 randomInt = randomRIO (0, maxBound :: Int)
+
+randomDouble :: IO Double
+randomDouble = do
+    value <- randomRIO (0.0, 1.0 :: Double)
+    if value == 1.0
+        then randomDouble
+        else return value
 
 
 -- Endpoint handlers
@@ -30,9 +42,15 @@ randomIntHandler = do
     val <- liftIO randomInt
     json $ RandomIntResponse val
 
+randomDoubleHandler :: ActionM ()
+randomDoubleHandler = do
+    val <- liftIO randomDouble
+    json $ RandomDoubleResponse val
+
 -- Main
 main :: IO ()
 main = scotty 3000 $ do
     middleware simpleCors
 
     get "/3.0" randomIntHandler
+    get "/3.5" randomDoubleHandler
