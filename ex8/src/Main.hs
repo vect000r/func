fbba3a1d988f62c9693b5883656340a5c1db643a
vect0r <vectoreddd@gmail.com>
2026-01-06 22:24:46 +0100

    feat: ex8 3.5 first version

--- ex8/src/Main.hs
+++ ex8/src/Main.hs
@@ -7,8 +7,13 @@ import Web.Scotty
 import Data.Aeson ( FromJSON, ToJSON, object, (.=), toJSON)
 import GHC.Generics
 import Network.Wai.Middleware.Cors
-import System.Random
 import Control.Monad.IO.Class (liftIO)
+import Data.Monoid (Sum(..), Product(..))
+import Data.Foldable (foldMap)
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text as T
+import Text.Read (readMaybe)
+
 
 -- Response types
 data OperationResult = OperationResult
@@ -16,6 +21,24 @@ data OperationResult = OperationResult
   } deriving (Generic, Show)
 
 instance ToJSON OperationResult
+
+data ConcatResult = ConcatResult
+  { concatenated :: [Int]
+  } deriving (Generic, Show)
+
+instance ToJSON ConcatResult
+
+
+-- Helper functions
+parseIntList :: String -> Maybe [Int]
+parseIntList str = mapM readMaybe (splitOn ',' str)
+  where
+    splitOn :: Char -> String -> [String]
+    splitOn _ [] = []
+    splitOn delim s = case break (== delim) s of
+      (before, []) -> [before]
+      (before, _:after) -> before : splitOn delim after
+
 
 -- Endpoint handlers
 operationHandler :: ActionM ()
@@ -37,7 +60,19 @@ operationHandler = do
     Just r -> json $ OperationResult r
     Nothing -> json $ OperationResult 0
 
+concatHandler :: ActionM ()
+concatHandler = do
+  list1Str <- param "list1" :: ActionM String
+  list2Str <- param "list2" :: ActionM String
+  list3Str <- param "list3" :: ActionM String
+  
+  let list1 = maybe [] id (parseIntList list1Str)
+      list2 = maybe [] id (parseIntList list2Str)
+      list3 = maybe [] id (parseIntList list3Str)
+  
+  let concatenatedList = list1 <> list2 <> list3
 
+  json $ ConcatResult concatenatedList
 
 
 main :: IO ()
@@ -45,3 +80,4 @@ main = scotty 3000 $ do
   middleware simpleCors 
 
   get "/3.0" operationHandler  
+  get "/3.5" concatHandler
