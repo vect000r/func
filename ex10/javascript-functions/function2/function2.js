43c64455b10500aa40157b477de66cb16e456cd2
vect0r <vectoreddd>
2026-02-03 20:25:56 +0100

    feat: function2 created

---
+++ ex10/javascript-functions/function2/function2.js
@@ -0,0 +1,18 @@
+const { app } = require("@azure/functions");
+
+function isZero(num) {
+    return num == 0;
+}
+
+app.http("function2", {
+    methods: ["POST"],
+    authLevel: "anonymous",
+    handler: async (request, context) => {
+        const { num } = await request.json();
+
+        return {
+            status: 200,
+            jsonBody: { isZero: isZero(num) },
+        };
+    },
+});
